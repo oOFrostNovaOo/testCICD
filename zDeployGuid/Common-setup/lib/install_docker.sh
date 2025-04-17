519d2498724e8232b6a1592b48b53b3d14207965
boydoncoi <boydoncoi@gmail.com>
2025-04-17 14:26:52 +0700

    update install_docker.sh

--- zDeployGuid/Common-setup/lib/install_docker.sh
+++ zDeployGuid/Common-setup/lib/install_docker.sh
@@ -18,8 +18,7 @@
 function implement_Docker_swarm() {
     # Lấy IP từ nhóm [leader] trong inventory
     leader_ip=$(ansible leader -i inventory.ini -m setup -a 'filter=ansible_default_ipv4.address' | grep ansible_default_ipv4.address | awk '{print $2}')
-    echo "Leader IP: $leader_ip"
-
+    
     # Install Docker trên tất cả các node
     echo "📦 Installing Docker..."
     ansible-playbook -i inventory.ini playbooks/install_docker.yml
@@ -29,9 +28,12 @@ function implement_Docker_swarm() {
 
     echo "📤 Extracting token and saving to swarm_token.yml..."
     token=$(cat swarm_token.txt)
-    echo "swarm_token: $token" > playbooks/swarm_token.yml
-    echo "leader_ip: 192.168.1.10" >> playbooks/swarm_token.yml
+    echo "swarm_token: \"$token\"" > playbooks/swarm_token.yml
+    echo "leader_ip: \"$leader_ip\"" >> playbooks/swarm_token.yml
 
     echo "🔗 Joining Workers to Swarm..."
     ansible-playbook -i inventory.ini playbooks/join_swarm.yml
+    echo "✅ Docker Swarm setup completed."
+    read -p "Press any key to continue..."
+    echo "-----------------------------------------------"
 }
